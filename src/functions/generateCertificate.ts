6911efaa058f5d37b741d0e5bce10edc9f8db60e
Rodrigo Schieck <rodrigoschieck.pro@gmail.com>
2022-04-16 15:24:59 -0300

    serverless: Generate certificates locally.

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,31 @@
+import  dayjs from "dayjs";
+import  chromium from "chrome-aws-lambda";
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from "../utils/dynamodbClient";
+import { compile } from "handlebars";
+import { join } from "path";
+import { readFileSync } from "fs";
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
+}
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = readFileSync(filePath, "utf8");
+
+  return compile(html)(data);
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -31,6 +52,38 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       },
     })
     .promise();
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal
+  }
+
+  const content = await compileTemplate(data);
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+  await page.pdf({
+    format: "a4",
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.IS_OFFLINE ? "./certificate.pdf" : null 
+  });
+
+  await browser.close();
 
   return {
     statusCode: 201,
